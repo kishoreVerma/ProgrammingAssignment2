8870d9d9cfc19f10bc065af19bdd69f9d88ddbce
kishoreVerma <Kishore_verma@rediffmail.com>
2017-01-03 17:38:01 +0530

    updated the code with comments

--- Programming Assignment 2-Lexical Scoping.R
+++ Programming Assignment 2-Lexical Scoping.R
@@ -1,22 +1,21 @@
-## makeCacheMatrix function creates a special "matrix" object that can cache its inverse
+# makeCacheMatrix function creates a special "matrix" object that can cache its inverse
 ## cacheSolve function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
 ## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
-makeCacheMatrix <- function(x = matrix())
-{
-  # Inverse cached matrix
-  inv_mat <- NULL
+makeCacheMatrix <- function(x = matrix()){
+
+    inv_mat <- NULL
   
-  # set the matrix
-  set <- function(y) 
-  {
-    x <<- y
-    
-    # make the inverse matrix null if its a new matrix
-    inv_mat <<- NULL
-  }
     # set the matrix
-    
+    set <- function(y){
+      
+      x <<- y    
+      
+      # make the inverse matrix null if its a new matrix   
+      inv_mat <<- NULL
+    }
+     
+    # set the matrix
     get <- function() x
     
     # set the inverse of the matrix  
@@ -32,30 +31,25 @@ makeCacheMatrix <- function(x = matrix())
 ## cacheSolve function computes the inverse of the special "matrix" returned by makeCacheMatrix.
 ## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...)
-
-{
-
+cacheSolve <- function(x, ...){
+    
     inv_mat <- x$getinv()
   
-    #return the inverse matrix form cache If inverse matrix is already calculated
+    #return the inverse matrix form cache If inverse matrix is already there
     
-    if (!is.null(inv_mat)) 
-  
-      {
+    if (!is.null(inv_mat)){
+        # print message if its cached inverse matrix  & return the inverse cached matrix   
         message("getting cached matrix")
         return(inv_mat)
+      
       } 
     
-    else 
-    
-      {
+    else{
+      
         #If it is a new matrix then calculate matrix inverse and cache it
+    
         inv_mat <- solve(x$get())
-      
         x$setinv(inv_mat)
-      
         return(inv_mat)
-      
       }
 }
